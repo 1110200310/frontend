5e0d553b1fe8f71b53a184223441057025c8c5a2
1110200310 <maiduythang2k3@gmail.com>
2025-03-13 11:32:14 +0700

    frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,53 +1,55 @@
-const listImage =document.querySelector('.list-images')
-const imgs = document.getElementsByTagName('img')
-const btnLeft = document.querySelector('.btn-left')
-const btnRight = document.querySelector('.btn-right')
-const length = imgs.length
-let current = 0
 
-const handleChangeSlide = () =>{
-    if (current == length -1 ){
-        current = 0
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(0px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-' + current).classList.add('active')
-    } else {
-        current ++
-        let width =  imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 * current}px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-' + current).classList.add('active')
-    }
-}
 
-let handleEventChangeSlide = setInterval(handleChangeSlide,4000)
+// const listImage =document.querySelector('.list-images')
+// const imgs = document.getElementsByTagName('img')
+// const btnLeft = document.querySelector('.btn-left')
+// const btnRight = document.querySelector('.btn-right')
+// const length = imgs.length
+// let current = 0
 
-btnRight.addEventListener('click', () => {
-    clearInterval(handleEventChangeSlide)
-    handleChangeSlide()
-    handleEventChangeSlide = setInterval(handleChangeSlide,4000)
-})
+// const handleChangeSlide = () =>{
+//     if (current == length -1 ){
+//         current = 0
+//         let width = imgs[0].offsetWidth
+//         listImage.style.transform = `translateX(0px)`
+//         document.querySelector('.active').classList.remove('active')
+//         document.querySelector('.index-item-' + current).classList.add('active')
+//     } else {
+//         current ++
+//         let width =  imgs[0].offsetWidth
+//         listImage.style.transform = `translateX(${width * -1 * current}px)`
+//         document.querySelector('.active').classList.remove('active')
+//         document.querySelector('.index-item-' + current).classList.add('active')
+//     }
+// }
 
-btnLeft.addEventListener('click',() => {
-    clearInterval(handleEventChangeSlide)
-    if (current == 0 ){
-        current = length - 1
-        let width = imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 *current}px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-' + current).classList.add('active')
+// let handleEventChangeSlide = setInterval(handleChangeSlide,4000)
 
-    } else {
-        current --
-        let width =  imgs[0].offsetWidth
-        listImage.style.transform = `translateX(${width * -1 * current}px)`
-        document.querySelector('.active').classList.remove('active')
-        document.querySelector('.index-item-' + current).classList.add('active')
-    }
-    handleEventChangeSlide = setInterval(handleChangeSlide,4000)
-})
-// Trượt sản phẩm>
+// btnRight.addEventListener('click', () => {
+//     clearInterval(handleEventChangeSlide)
+//     handleChangeSlide()
+//     handleEventChangeSlide = setInterval(handleChangeSlide,4000)
+// })
+
+// btnLeft.addEventListener('click',() => {
+//     clearInterval(handleEventChangeSlide)
+//     if (current == 0 ){
+//         current = length - 1
+//         let width = imgs[0].offsetWidth
+//         listImage.style.transform = `translateX(${width * -1 *current}px)`
+//         document.querySelector('.active').classList.remove('active')
+//         document.querySelector('.index-item-' + current).classList.add('active')
+
+//     } else {
+//         current --
+//         let width =  imgs[0].offsetWidth
+//         listImage.style.transform = `translateX(${width * -1 * current}px)`
+//         document.querySelector('.active').classList.remove('active')
+//         document.querySelector('.index-item-' + current).classList.add('active')
+//     }
+//     handleEventChangeSlide = setInterval(handleChangeSlide,4000)
+// })
+// Trượt ưu đãi hót>
 
 document.addEventListener("DOMContentLoaded", function () {
     const btns = document.querySelector('.btns'); 
